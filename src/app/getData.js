e301c40eead8ddc51c678fce1746ca5b9fc33591
Michal Kowalczyk <mickowalczyk6@gmail.com>
2021-12-05 19:26:19 +0100

    add fetch api

---
+++ src/app/getData.js
@@ -0,0 +1,36 @@
+export const getCategories = async () => {
+  try{
+    const res = await fetch(`https://opentdb.com/api_category.php`);
+    if(!res.ok) throw new Error(res.statusText);
+    const data = await res.json();
+    sessionStorage.setItem('categories', JSON.stringify(data.trivia_categories));
+    return data;
+  } catch {
+    console.error(Error);
+  }
+};
+
+export const getQuiz = async (categoryID) => {
+  try{
+    let path;
+    if(categoryID === 999){
+      path = 'https://opentdb.com/api.php?amount=10';
+    } else {
+      path = `https://opentdb.com/api.php?amount=10&category=${categoryID}`;
+    }
+    const res = await fetch(path);
+
+    if(!res.ok) throw new Error(res.statusText);
+    const data = await res.json();
+    return data;
+  } catch {
+    console.error(Error);
+  }
+};
+
+// export const getQuiz = (categoryID) => {
+//   fetch(`https://opentdb.com/api_count.php?category=${categoryID}`)
+//   .then(res => res.json())
+//   .then(data => console.log(data))
+// }
+// jak zrobić return z tego ?
