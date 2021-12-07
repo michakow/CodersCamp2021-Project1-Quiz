de5aa32f2fd673bf861767530d9fd57195055463
Michał Kowalczyk <70285236+michakow@users.noreply.github.com>
2021-12-07 19:00:47 +0100

    add fetch api (#3)
    * add fetch api
    
    * fix error handling
    
    * format code with Prettier

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,11 @@
+import { getCategories, getQuiz } from './getData.js';
 // Here goes logic:
 console.log('Hello world');
+
+const quiz = getQuiz(12);
+quiz.then((value) => console.log(value));
+
+const categories = sessionStorage.getItem('categories')
+  ? JSON.parse(sessionStorage.getItem('categories'))
+  : getCategories();
+console.log(categories);
