c1606b0a24e3a03bc24c9e6f396c8d685fd1a370
Magdalena Zaniewska-Ciecierska <magdalenaz-c@MacBook-Air-Magdalena.local>
2021-12-19 22:43:45 +0100

    fix window.points issue

--- src/app/startQuiz.js
+++ src/app/startQuiz.js
@@ -3,18 +3,17 @@ import { finishQuiz } from './finishQuiz';
 
 
 export const startQuiz = async (questionList) => {
-  let points = 0;
   const questionLength = questionList.length;
+  let points = 0;
 
   const gameInner = document.getElementsByClassName('game__inner')[0];
   gameInner.innerHTML = "";
   //TODO startTimer();
   for (let [id, question] of questionList.entries()) {
     const answer = await renderQuestion(id, question, questionLength);
-    if (answer) window.points++;
+    if (answer) points++;
   
     if (id === questionLength - 1) {
-      console.log('test')
       return finishQuiz(window.userName, points, 'music')
     }
   }
