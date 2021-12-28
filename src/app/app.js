99d9e2cdc2d6c810a14c54661826fc5c3de4b1fc
Roman Pavlenko <rpavlenko.mail@gmail.com>
2021-12-28 21:09:50 +0100

    add scroll, style, and other media fixes

--- src/app/app.js
+++ src/app/app.js
@@ -3,7 +3,7 @@ import { homepage } from './homepage.js';
 import { showLeaderboard } from './leaderboard.js';
 // import { chooseCategory } from './chooseCategory.js';
 import { startQuiz } from './startQuiz.js';
-import {validateUserName} from './tools.js';
+import { validateUserName } from './tools.js';
 
 window.userName = '';
 
@@ -15,13 +15,14 @@ export const startApp = async (id, name) => {
   div.innerHTML = `
     <section class="game">
       <div class="container game__inner">
+        <div class="game__header">
         <input class="game__user--name" placeholder="Enter your name..." />
-
         <button class="button game__button--start-quiz">Start Quiz</button>
         <button class="button game__button--scores">Scores</button>
         <p class="game__questions-quantity">
           Total questions for ${categoryName}: 245
         </p>
+        </div>
         <button class="button game__button--back">Back to categories</button>
       </div>
     </section>
@@ -45,16 +46,19 @@ export const startApp = async (id, name) => {
       const userInput = document.querySelector('.game__user--name');
       window.userName = userInput.value;
       const isValidUser = validateUserName(window.userName);
-      if(isValidUser) {
-         startQuiz(questionList, categoryName);
-      }
-      else {
+      if (isValidUser) {
+        startQuiz(questionList, categoryName);
+      } else {
         if (errorText) {
           return;
         }
         errorText = document.createElement('div');
         errorText.classList.add('game__error');
-        errorText.appendChild(document.createTextNode('User Name should consist of min 3 LETTERS, and letters only.'));
+        errorText.appendChild(
+          document.createTextNode(
+            'User Name should consist of min 3 LETTERS, and letters only.',
+          ),
+        );
         userInput.parentNode.insertBefore(errorText, userInput.nextSibling);
       }
     },
