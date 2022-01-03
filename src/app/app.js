52e31ffaa6e105eda03c2390a9d33942c432088c
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-01-03 20:11:03 +0100

    add loader for startApp screen, fix logo link to github-pages

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,9 @@
 import { getQuestions, getQuestionTotalCount } from './getData.js';
 import { homepage } from './homepage.js';
 import { showLeaderboard } from './leaderboard.js';
-// import { chooseCategory } from './chooseCategory.js';
 import { startQuiz } from './startQuiz.js';
 import { validateUserName } from './tools.js';
+import { loader } from './loader.js';
 
 window.userName = '';
 
@@ -11,17 +11,18 @@ export const startApp = async (id, name) => {
   const categoryID = id;
   const categoryName = name;
 
+  loader();
   const questionCount = await getQuestionTotalCount(categoryID);
 
   let questionCountForLevel;
-  switch(window.questionsLevel){
-    case 'hard': 
+  switch (window.questionsLevel) {
+    case 'hard':
       questionCountForLevel = questionCount.total_hard_question_count;
       break;
     case 'medium':
       questionCountForLevel = questionCount.total_medium_question_count;
       break;
-    default: 
+    default:
       questionCountForLevel = questionCount.total_easy_question_count;
   }
 
@@ -35,8 +36,12 @@ export const startApp = async (id, name) => {
         <button class="button game__button--scores">Scores</button>
         <p class="game__questions-quantity">
           <span class="game__category-name">${categoryName}</span>
-          <span class="game__total-questions">Total questions: ${questionCount.total_question_count}</span>
-          <span class="game__total-questions-for-level">Questions for ${window.questionsLevel || 'easy'} level: ${questionCountForLevel}</span>
+          <span class="game__total-questions">Total questions: ${
+            questionCount.total_question_count
+          }</span>
+          <span class="game__total-questions-for-level">Questions for ${
+            window.questionsLevel || 'easy'
+          } level: ${questionCountForLevel}</span>
         </p>
         </div>
         <button class="button game__button--back">Back to categories</button>
@@ -58,7 +63,6 @@ export const startApp = async (id, name) => {
   startButton.addEventListener(
     'click',
     () => {
-      //TODO validate username
       const userInput = document.querySelector('.game__user--name');
       window.userName = userInput.value;
       const isValidUser = validateUserName(window.userName);
--- src/app/loader.js
+++ src/app/loader.js
@@ -1,6 +1,5 @@
 export const loader = () => {
   const parent = document.querySelector('#app');
   const loader = `<div class="lds-dual-ring"></div>`;
-
   parent.innerHTML = loader;
 };
